0fc42d15fff9725db53f354de7d4bd90414ab606
Aldik666 <iamajesty456@gmail.com>
2025-10-08 11:02:41 +0500

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,17 @@
+int main() {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(0, &readfds); // stdin
+
+    struct timeval tv = {5, 0}; // 5 секунд күту
+
+    printf("5 секунд ішінде енгізу күтілуде...\n");
+    int ret = select(1, &readfds, NULL, NULL, &tv);
+
+    if (ret > 0)
+        printf("Енгізу анықталды!\n");
+    else
+        printf("Уақыт аяқталды.\n");
+
+    return 0;
+}
